a318874726157139714511be1f7dee9cd7a79b18
Lilitha27 <lilithakganya@gmail.com>
2019-10-24 12:34:57 +0200

    added files and folders

--- js/main.js
+++ js/main.js
@@ -1,67 +1,137 @@
-function navBar() {
-    var x = document.getElementById("myTopnav");
-    if (x.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
+
+( addItemsToCart = () =>{
+let buttons = document.querySelectorAll('[data-add-btn]');
+console.log(buttons.length);
+for(let i=0; i<buttons.length; i++ ){
+    oneButton = buttons[i];
+    count = 0;
+    oneButton.addEventListener('click', (event)=>{
+        console.log("clicked add t cart button");
+        clickedButton = event.target;
+       shopItemItemDiv = clickedButton.parentElement;
+       console.log(shopItemItemDiv);
+       itemTitle = shopItemItemDiv.firstElementChild.textContent;
+        console.log(itemTitle);
+       itemPrice = shopItemItemDiv.firstElementChild.nextElementSibling.textContent;
+       console.log(itemPrice);
+       priceElement = shopItemItemDiv.firstElementChild.nextElementSibling;
+       itemDescription = priceElement.nextElementSibling.textContent;
+       console.log(itemDescription);
+       title = document.createElement('h4');
+       title.textContent = itemTitle;
+       price = document.createElement('p');
+       price.textContent = itemPrice;
+       description = document.createElement('p');
+       description.textContent = itemDescription;
+       removeButton = document.createElement('button');
+       removeButton.classList.add('remove-button');
+       removeButton.textContent = "Remove Item";
+        console.log(title);
+        console.log(price);
+        console.log(description);
+
+       description = itemDescription;
+       itemContainer =document.createElement('div');
+       itemContainer.classList.add('basket-item-container');
+       itemContainer.appendChild(title);
+       itemContainer.appendChild(price);
+       itemContainer.appendChild(removeButton);
+      //  itemContainer.appendChild(description);
+         basket = document.querySelector('.basket');
+       basket.appendChild(itemContainer);
+       count+= 1;
+       shoppingCart=document.querySelector('#cart-total');
+       shoppingCart.textContent = count;
+       //display items added to cart
+       basket.style.display = "block";
+       //increase the cart total
+     
+     
+
+
+
+       console.log(itemContainer);
+       console.log(basket);
+       displayCartTotal();
+       removeItemFromCart();
+       
+       
+    })
+}
+//Update cart total function
+const displayCartTotal = () =>{
+  cartItems = document.querySelectorAll('.basket-item-container');
+  cartTotal = document.querySelector('.cart-total');
+  console.log(cartItems);
+  total = 0;
+  for(let i=0; i<cartItems.length; i++){
+    oneCartItem = cartItems[i];
+    priceTag = oneCartItem.firstElementChild.nextElementSibling.textContent;
+    priceTagFormatted = priceTag.substr(1,4);
+    console.log(priceTagFormatted);
+    total += Number(priceTagFormatted);
+    cartTotal.textContent = total;
+  }
+};
+//Remove Item From Cart Function
+const removeItemFromCart = () =>{
+  cartItemDivs = document.querySelectorAll('.basket-item-container');
+  for(let i=0; i<cartItemDivs.length; i++){
+    oneCartItemDiv = cartItemDivs[i];
+    removeButton = oneCartItemDiv.lastElementChild;
+    removeButton.addEventListener('click', (event) =>{
+      clickedRemoveButton = event.target;
+      cartItem = clickedRemoveButton.parentElement;
+      cartItem.remove();
+      updateCartTotal();
+      
+    });
   }
 
- 
- 
- 
- 
- 
- 
-  function myFunction() {
-    var dots = document.getElementById("dots");
-    var moreText = document.getElementById("more");
-    var btnText = document.getElementById("myBtn");
-  
-    if (dots.style.display === "none") {
-      dots.style.display = "inline";
-      btnText.innerHTML = "Read more"; 
-      moreText.style.display = "none";
-    } else {
-      dots.style.display = "none";
-      btnText.innerHTML = "Read less"; 
-      moreText.style.display = "inline";
-    }
-  }  
+};
+const updateCartTotal = () =>{
+      let basket = document.querySelector('.basket');
+      let basketItemContainers = basket.querySelectorAll('basket-item-container');
+      let total = 0;
+      let grandTotal =parseFloat(document.querySelector('.cart-total').innerText);
+      for(let i =0; i<basketItemContainers.length; i++){
+        basketItemContainer = basketItemContainers[i];
+        priceElement = basketItemContainer.querySelector('.price-tag');
+        price = parseFloat(priceElement.innerText);
+        console.log(price);
+        total = total + price;
+        
+      }
+        currentTotal = parseFloat(document.querySelector('.cart-total').innerText);
+        grandTotal = currentTotal - total;
+        document.querySelector('.cart-total').innerText =grandTotal;
+      
+};
+
+   
+})();
+
+
 // lookbook
 
-<script>
-function myFunction(images) {
+function lookbook(images) {
   var expandImg = document.getElementById("expandedImg");
   var imgText = document.getElementById("imgtext");
   expandImg.src = imgs.src;
   imgText.innerHTML = imgs.alt;
   expandImg.parentElement.style.display = "block";
 }
-</script>
 
-//  scroll arrow
-$(document).ready(function(){
-  // Add smooth scrolling to all links
-  $("a").on('click', function(event) {
+// nav
+window.onscroll = function() {navigation()};
 
-    // Make sure this.hash has a value before overriding default behavior
-    if (this.hash !== "") {
-      // Prevent default anchor click behavior
-      event.preventDefault();
+var navbar = document.getElementById("navbar");
+var sticky = navbar.offsetTop;
 
-      // Store hash
-      var hash = this.hash;
-
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 800, function(){
-   
-        // Add hash (#) to URL when done scrolling (default click behavior)
-        window.location.hash = hash;
-      });
-    } // End if
-  });
-});
+function navigation() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
